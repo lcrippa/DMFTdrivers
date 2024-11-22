1679a4bb7b25a0c6cc9f7b35382aed10834111e3
lcrippa <crippa.lorenzo@gmail.com>
2024-11-22 11:22:54 +0100

    Update 22.11.2024

--- cosine_bethe_test/diag_test.f90
+++ cosine_bethe_test/diag_test.f90
@@ -20,7 +20,6 @@ program realistic_soc
   logical                                       :: mixG0
   
   character(len=32)                             :: finput
-  character(len=32)                             :: parametrization 
   
   
   real(8),allocatable                           :: Bath(:),Bath_(:)
@@ -54,7 +53,6 @@ program realistic_soc
 
   
   call parse_cmd_variable(finput,"FINPUT",default='input.in')
-  call parse_input_variable(parametrization,"PARAMETRIZATION",finput,default='soc')
   call parse_input_variable(wmixing,"wmixing",finput,default=0.5d0,comment="Mixing bath parameter")
   call parse_input_variable(Nx,"Nx",finput,default=100,comment="Number of kx point for 2d BZ integration")
   call parse_input_variable(mixG0,"mixG0",finput,default=.false.)
@@ -90,12 +88,12 @@ program realistic_soc
   allocate(u_jbasis(6,6));u_jbasis=zero
   allocate(spinorbit_matrix_t2g_nn(Nspin,Nspin,3,3));spinorbit_matrix_t2g_nn=zero
   !
-  spinorbit_matrix_t2g(1,:) = (-0.5)*[  zero,   -xi,   zero,   zero,  zero,   one]
-  spinorbit_matrix_t2g(2,:) = (-0.5)*[    xi,  zero,   zero,   zero,  zero,   -xi]
-  spinorbit_matrix_t2g(3,:) = (-0.5)*[  zero,  zero,   zero,   -one,    xi,  zero]
-  spinorbit_matrix_t2g(4,:) = (-0.5)*[  zero,  zero,   -one,   zero,    xi,  zero]
-  spinorbit_matrix_t2g(5,:) = (-0.5)*[  zero,  zero,    -xi,    -xi,  zero,  zero]  
-  spinorbit_matrix_t2g(6,:) = (-0.5)*[   one,   xi,    zero,   zero,  zero,  zero] 
+  spinorbit_matrix_t2g(1,:) = (-0.5)*[  zero,   one,   zero,   zero,  zero,   one]
+  spinorbit_matrix_t2g(2,:) = (-0.5)*[   one,  zero,   zero,   zero,  zero,   one]
+  spinorbit_matrix_t2g(3,:) = (-0.5)*[  zero,  zero,   zero,    one,   one,  zero]
+  spinorbit_matrix_t2g(4,:) = (-0.5)*[  zero,  zero,    one,   zero,   one,  zero]
+  spinorbit_matrix_t2g(5,:) = (-0.5)*[  zero,  zero,    one,    one,  zero,  zero]  
+  spinorbit_matrix_t2g(6,:) = (-0.5)*[   one,   one,   zero,   zero,  zero,  zero] 
   spinorbit_matrix_t2g_nn = so2nn(spinorbit_matrix_t2g,2,3)
   !
   u_jbasis(1,:) = (1.0/sqrt(6d0))*[ -one*sqrt(3d0),     one,          zero,  zero,           zero, -one*sqrt(2d0)]
@@ -129,6 +127,9 @@ program realistic_soc
   !Build Hk and Hloc for impurity and lattice
   call set_hamiltonian() 
   
+  print*,"Hloc is"
+  call print_c_matrix(nn2so(Hloc,Nspin,Norb))
+  
   !Setup Bath
   call set_bath()
   
@@ -148,7 +149,6 @@ program realistic_soc
     call ed_get_sigma(Smats,axis='mats')
     call ed_get_sigma(Sreal,axis='real')
     s0=Smats(:,:,:,:,1)
-    call get_Zmats()
     
     !Compute the local gfs on the imaginary axis:
     call get_glocal()
@@ -345,16 +345,10 @@ contains
     
   subroutine set_bath()
 
-    if(bath_type == "replica" .or. bath_type == "general")then
-      if (parametrization == "soc")then
-        call parametrization_soc()
-      elseif(parametrization == "diagonal")then
-        call parametrization_diagonal()
-      else
-        STOP
-      endif
+    if(bath_type =="replica" .or. bath_type =="general")then
+      call parametrization_symmetric()
 
-      if(bath_type == "replica")then
+      if(bath_type =="replica")then
         call ed_set_Hreplica(Hsym_basis,lambdasym_vectors)
       else
         call ed_set_Hgeneral(Hsym_basis,lambdasym_vectors)
@@ -373,40 +367,7 @@ contains
 
   end subroutine set_bath
     
-  subroutine parametrization_diagonal()
-    integer              :: irepl,onsite
-        
-    allocate(tmpmat(Nso,Nso))
-    NsymMats=1
-
-    allocate(lambdasym_vectors(Nbath,NSymMats)); lambdasym_vectors=zero
-    allocate(Hsym_basis(Nspin,Nspin,Norb,Norb,NSymMats)); Hsym_basis=zero
-    allocate(Hsym_t2g(2,2,3,3,NSymMats)); Hsym_t2g=zero
-
-    !symmetry 1: diagonal
-    Hsym_t2g(:,:,:,:,1) = so2nn(zeye(3*Nspin),nspin,3)
-
-    !cut out the block
-    Hsym_basis = Hsym_t2g(:,:,1:Norb,1:Norb,:)
-
-    
-    write(*,*) "Replica initialization: ed_hw_bath="//str(ed_hw_bath)
-
-    do irepl=1,Nbath
-      onsite = irepl -1 - (Nbath-1)/2d0          ![-(Nbath-1)/2:(Nbath-1)/2]
-      if(Nbath>1) onsite = onsite * 2*ed_hw_bath/(Nbath-1)   !P-H symmetric band, -ed_hw_bath:ed_hw_bath
-      lambdasym_vectors(irepl,1) = onsite        !Multiplies the suitable identity
-    enddo
-
-
-    if(mod(Nbath,2)==0 .and. Nbath>2)then
-      lambdasym_vectors(Nbath/2,1) = -1d-1    !Much needed small energies around
-      lambdasym_vectors(Nbath/2+1,1) = 1d-1   !the fermi level. (for even Nbath)
-    endif    
-
-  end subroutine parametrization_diagonal
-  
-  subroutine parametrization_soc()
+  subroutine parametrization_symmetric()
     integer              :: irepl,onsite
         
     allocate(tmpmat(Nso,Nso))
@@ -444,7 +405,7 @@ contains
       lambdasym_vectors(Nbath/2+1,1) = 1d-1   !the fermi level. (for even Nbath)
     endif    
 
-  end subroutine parametrization_soc
+  end subroutine parametrization_symmetric
   
   !+---------------------------------------------------------------------------+
   !get glocal
@@ -513,7 +474,6 @@ contains
 
   subroutine get_zmats()
     complex(8),dimension(Nso,Nso) :: s0so,Uinv,tmpmat,s0nh
-    integer :: icomp
     !
     !
     Zmats=zero
@@ -527,7 +487,7 @@ contains
     !endif
     !
     s0nh = (s0so-conjg(transpose(s0so)))/(2*xi)
-    tmpmat = zeye(Nso)-s0nh/(pi/beta)
+    tmpmat = zeye(6)-s0nh/(pi/beta)
     !tmpmat = zeye(6) - imag(s0so)/(pi/beta)
     call inv(tmpmat)
     Zmats=real(tmpmat)
@@ -535,7 +495,7 @@ contains
     if(master)then
       unit=free_unit()
       open(unit,file="z.dat",position='append')
-      write(unit,"(12(F12.8))")(Zmats(icomp,icomp),icomp =1,Nso)
+      write(unit,*)Zmats(1,1),Zmats(2,2),Zmats(3,3),Zmats(4,4),Zmats(5,5),Zmats(6,6)
       close(unit)
       print*,"print Zmats"
       call print_a_matrix(Zmats)
